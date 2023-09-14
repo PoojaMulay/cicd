32152dc240e990044f3b7ee89546b59101cb4e2b
PoojaMulay <119414915+PoojaMulay@users.noreply.github.com>
2023-09-14 18:45:22 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "primuslearning-app"
     region = "us-east-1"
-    key = "jenkins-server/terraform.tfstate"
+    Profile = "my_default_profile_name"
   }
 }
